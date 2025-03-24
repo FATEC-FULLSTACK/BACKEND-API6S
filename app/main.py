7c58a7c0b63afe5db2f29b3bb10ba957afeb0e23
MateusdiSousa <mateus69107@gmail.com>
2025-03-24 07:36:40 -0300

    feat: FAT-9: rotas de avaliação e correção de imports

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from app.routes import chat_router
-from app.config import load_environment
+from routes.chat_router import chat_router
+from routes.avaliacao_router import avalicao_router
+from config import load_environment
 
 # Carrega as variáveis de ambiente
 load_environment()
 
 app = FastAPI(title="API6S")
 
-# Inclui as rotas do chat
+# Inclui as rotas do chat e avaliacao
 app.include_router(chat_router)
+app.include_router(avalicao_router)
