a4faf316d8a1ec55ff23dee0bf13a01a641380da
MateusdiSousa <mateus69107@gmail.com>
2025-03-24 07:35:57 -0300

    feat: FAT-9: conexao com o banco de dados

---
+++ app/database/config.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+from config import MONGO_URL
+
+client = MongoClient(MONGO_URL)
+
+db = client.get_database("API6")
+
+avaliacao_collection = db.get_collection("avaliacao")
