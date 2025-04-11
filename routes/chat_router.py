62e7c1bdfdb16012642d0126e444445d9ef161ed
Juliano <julianoprado98@gmail.com>
2025-04-11 08:41:46 -0300

    Revisando o contexto da LLM

---
+++ routes/chat_router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, HTTPException
+from app.models.models import ChatRequest
+from app.services.chat import process_chat 
+from typing import Any
+
+chat_router = APIRouter()
+
+@chat_router.post("/chat", response_model=dict)
+async def chat(request: ChatRequest) -> Any:
+    try:
+        response = await process_chat(request)
+        return {"status": "success", "data": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro interno: {str(e)}")
---
+++ services/avaliacao.py
@@ -0,0 +1,51 @@
+from app.schema.schemas import individual_serial
+from app.database.config import avaliacao_collection
+from app.models.models import AvaliacaoModel, NotasAtributos, ParseAvaliacaoModelToDocument
+from bson import ObjectId
+from app.utils.validacao import validar_atributos
+
+llms_disponiveis = ["ollama3", "openai", "gemini", "deepseek"]
+
+async def PostAvaliacao(avaliacao: AvaliacaoModel):
+    if (avaliacao.llm1 not in llms_disponiveis or avaliacao.llm2 not in llms_disponiveis):
+        return {"message": "llms inválidas: llm1:{}, llm2:{}".format(avaliacao.llm1, avaliacao.llm2)}
+    if (not AtributoIsValid(avaliacao.avaliacao_llm1) or  not AtributoIsValid(avaliacao.avaliacao_llm2)):
+        return {"message": "notas dos atributos inválidas"}
+
+    avaliacao_collection.insert_one(ParseAvaliacaoModelToDocument(avaliacao=avaliacao))
+    return {"message": "Avaliacao criada com sucesso"}
+
+async def GetOneAvaliacao(id: str):
+    return individual_serial(avaliacao_collection.find_one({"_id":ObjectId(id)}))
+
+async def PutAvaliacao(id: str, avaliacao: AvaliacaoModel):
+    if (avaliacao.llm1 not in llms_disponiveis or avaliacao.llm2 not in llms_disponiveis):
+        return {"message": "llms inválidas: llm1:{}, llm2:{}".format(avaliacao.llm1, avaliacao.llm2)}
+    
+    if not AtributoIsValid(avaliacao.avaliacao_llm1) or not AtributoIsValid(avaliacao.avaliacao_llm2):
+        return {"message": "notas dos atributos inválidas"}
+    
+    result = avaliacao_collection.update_one(
+        {"_id": ObjectId(id)},
+        {"$set": ParseAvaliacaoModelToDocument(avaliacao=avaliacao)}
+    )
+    
+    if result.matched_count > 0:
+        return {"message": "Avaliação atualizada com sucesso"}
+    else:
+        return {"message": "Avaliação não encontrada"}
+    
+async def RemoveAvaliacao(id: str):
+    result = avaliacao_collection.delete_one({"_id": ObjectId(id)})
+    
+    if result.deleted_count > 0:
+        return {"message": "Avaliação deletada com sucesso"}
+    else:
+        return {"message": "Avaliação não encontrada"}
+
+def AtributoIsValid(avaliacao: NotasAtributos) -> bool:
+    try:
+        validar_atributos(avaliacao)
+        return True
+    except ValueError:
+        return False
