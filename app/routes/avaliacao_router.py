7e83525f108ed87338edb76b5ea3076b3a68f377
Leonardo Gazola Medeiros <leonardo.gazolamedeiros@gmail.com>
2025-05-24 18:24:48 -0300

    FAT-53: fix: alteração de logica para respostas em stream

--- app/routes/avaliacao_router.py
+++ app/routes/avaliacao_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.services.avaliacao import GetOneAvaliacao, PostAvaliacao, PutAvaliacao, RemoveAvaliacao
+from app.services.avaliacao import GetOneAvaliacao, PostAvaliacao, PutAvaliacao, RemoveAvaliacao, GetDesempenhoAvaliacao
 from app.models.models import AvaliacaoModel
 
 avaliacao_router = APIRouter(prefix="/avaliacao")
@@ -8,9 +8,9 @@ avaliacao_router = APIRouter(prefix="/avaliacao")
 async def CreateAvaliacao(avaliacao: AvaliacaoModel):
     return await PostAvaliacao(avaliacao=avaliacao)
 
-@avaliacao_router.get("/{id}")
-async def FindOneAvaliacao(id:str):
-    return await GetOneAvaliacao(id=id)
+# @avaliacao_router.get("/{id}")
+# async def FindOneAvaliacao(id:str):
+#     return await GetOneAvaliacao(id=id)
 
 @avaliacao_router.put("/{id}")
 async def UpdateAvaliacao(id: str, avaliacao: AvaliacaoModel):
@@ -18,4 +18,8 @@ async def UpdateAvaliacao(id: str, avaliacao: AvaliacaoModel):
 
 @avaliacao_router.delete("/{id}")
 async def DeleteAvaliacao(id: str):
-    return await RemoveAvaliacao(id=id)
+    return await RemoveAvaliacao(id=iavaliacoes_collectiond)
+
+@avaliacao_router.get("/desempenho")
+async def DesempenhoAvaliacao():
+    return await GetDesempenhoAvaliacao()
